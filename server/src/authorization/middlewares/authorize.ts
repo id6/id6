5041ced6e2f1486fef1cc79b4340f392480475c0
Geoffroy Empain <geoffroy@charlie-bravo.be>
2021-05-21 17:40:38 +0200

    fix: private api auth not working

--- server/src/authorization/middlewares/authorize.ts
+++ server/src/authorization/middlewares/authorize.ts
@@ -4,7 +4,7 @@ import { env } from '../../env/env';
 import { timingSafeEqual } from 'crypto';
 import { ErrorCode } from '@id6/commons/build/error-code';
 
-const secrets = Buffer.from(env.ID6_AUTHORIZATION_SECRET);
+const secret = Buffer.from(env.ID6_AUTHORIZATION_SECRET);
 
 export async function authorize(req: Request, res: Response, next: NextFunction) {
   const token: string = req.headers.authorization;
@@ -13,8 +13,10 @@ export async function authorize(req: Request, res: Response, next: NextFunction)
     return next(new UnauthorizedError('No token provided', ErrorCode.no_token_provided));
   }
 
-  if (timingSafeEqual(Buffer.from(token), secrets)) {
-    return next(new UnauthorizedError('Invalid token', ErrorCode.invalid_token));
+  const tokenBuffer = Buffer.alloc(secret.length, token);
+
+  if (!timingSafeEqual(tokenBuffer, secret)) {
+    return next(new UnauthorizedError('Invalid secret', ErrorCode.invalid_token));
   }
 
   next();
